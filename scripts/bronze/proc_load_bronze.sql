fb5be874ee175ccd6fc75990cde14ec7309126bb
mohamed-bengana <bengana083@gmail.com>
2025-08-02 23:08:08 +0100

    Update proc_load_bronze.sql

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -8,12 +8,6 @@ Script Purpose:
     - Truncates the bronze tables before loading data.
     - Uses the `BULK INSERT` command to load data from csv Files to bronze tables.
 
-Parameters:
-    None. 
-	  This stored procedure does not accept any parameters or return any values.
-
-Usage Example:
-    EXEC bronze.load_bronze;
 ===============================================================================
 */
 CREATE OR ALTER PROCEDURE bronze.load_bronze AS
