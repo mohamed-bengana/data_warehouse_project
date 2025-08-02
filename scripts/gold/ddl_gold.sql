cfc635f7bdcce3e63be4884911dc182f846e4f38
mohamed-bengana <bengana083@gmail.com>
2025-08-02 23:33:21 +0100

    Update ddl_gold.sql

--- scripts/gold/ddl_gold.sql
+++ scripts/gold/ddl_gold.sql
@@ -7,15 +7,8 @@ Script Purpose:
     The Gold layer represents the final dimension and fact tables (Star Schema)
 
     Each view performs transformations and combines data from the Silver layer 
-    to produce a clean, enriched, and business-ready dataset.
+    to produce a clean,  and business-ready dataset.
 
-Usage:
-    - These views can be queried directly for analytics and reporting.
-===============================================================================
-*/
-
--- =============================================================================
--- Create Dimension: gold.dim_customers
 -- =============================================================================
 IF OBJECT_ID('gold.dim_customers', 'V') IS NOT NULL
     DROP VIEW gold.dim_customers;
