18a99dea6ad1b2e27e0e0a975f4eac74fc94e61c
mohamed-bengana <bengana083@gmail.com>
2025-08-02 23:25:20 +0100

    Update proc_load_silver.sql

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -9,12 +9,6 @@ Script Purpose:
 		- Truncates Silver tables.
 		- Inserts transformed and cleansed data from Bronze into Silver tables.
 		
-Parameters:
-    None. 
-	  This stored procedure does not accept any parameters or return any values.
-
-Usage Example:
-    EXEC Silver.load_silver;
 ===============================================================================
 */
 
